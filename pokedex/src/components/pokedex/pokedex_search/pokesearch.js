236359835213da4c96fe73f2f06b17b3266a0c62
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-06-27 09:49:45 -0700

    added auto complete functionality added csv data fetch parsed csv into array got input value on change generated auto complete list and entries selected auto complete entry and put in input

--- pokedex/src/components/pokedex/pokedex_search/pokesearch.js
+++ pokedex/src/components/pokedex/pokedex_search/pokesearch.js
@@ -3,10 +3,76 @@ import './pokesearch.css';
 import {Link} from 'react-router-dom';
 
 class PokeSearch extends Component {
+	constructor(props){
+		super(props);
+		this.pokemonBaseData = [];
+		this.maxAutoCompleteEntries = 3;
+		this.state = {
+			inputVal: '', 
+			autoCompleteEntries: ''
+		}
+		this.updateInput = this.updateInput.bind(this);
+		this.useAutoCompleteEntry = this.useAutoCompleteEntry.bind(this);
+	}
+	parsePokeCSV(csvData){
+		const rows = csvData.split( csvData[ csvData.length-1]);
+		rows.shift(); //get rid of headers
+		this.pokemonBaseData = rows.map( item=> {
+			const stats = item.split(',');
+			return stats[1];
+			//return { name: stats[1], species_id: stats[2], height: stats[3]}
+		});
+		this.pokemonBaseData.sort();
+	}
+	componentDidMount(){
+		console.log('mounted');
+		fetch('pokemon.csv')
+			.then( (response)=>{ 
+				response.text().then( (data)=>{
+					this.parsePokeCSV(data);
+				})
+				
+			})
+			.catch( ()=>{ console.log('error')});
+	}
+	updateInput( event ){
+		const value = event.target.value;
+		const nextState = {
+			inputVal: value,
+			autoCompleteEntries: this.generatePokemonAutoCompleteEntries(value)
+		}
+		console.log(nextState);
+		this.setState(nextState);
+	}
+	useAutoCompleteEntry( event ){
+		const entryText = event.target.innerText;
+		this.setState({
+			inputVal: entryText,
+			autoCompleteEntries: ''
+		})
+	}
+	generatePokemonAutoCompleteEntries( partialText ){
+
+		if(partialText.length===0 || this.pokemonBaseData.length===0){
+			return '';
+		}
+		const regex = new RegExp('^'+partialText+'.*');
+		const candidateEntries = this.pokemonBaseData.filter( entry=> regex.test(entry)).slice(0,3);
+
+		return (<div className="autocomplete-container">
+									{(candidateEntries.map( (entry, index) => 
+											<div onClick={this.useAutoCompleteEntry} className="autocomplete-entry" key={index}>{entry}</div>
+									))}
+								</div>)
+
+	}
 	render(){
 		return (
 <div className="pokedex-search">
-	<input className="nameinput" type="text" placeholder="enter name"></input>
+	<div className="input-container">
+		<input onChange={this.updateInput} className="nameinput" type="text" placeholder="enter name" value={this.state.inputVal}></input>
+		{this.state.autoCompleteEntries}
+	</div>
 	<button className="searchbutton">SEARCH</button>
 	<Link to="/results">go to results</Link>
 </div>
